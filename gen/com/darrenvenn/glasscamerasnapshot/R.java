6a0c38b947dc7586850ae6746d95ac6590d9744d
Freddy Gu <freddy.gu@gmail.com>
2014-07-23 12:19:53 -0400

    Initial UI complete

--- gen/com/darrenvenn/glasscamerasnapshot/R.java
+++ gen/com/darrenvenn/glasscamerasnapshot/R.java
@@ -11,51 +11,89 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f070004;
+        /**  The standard margin used on the top, left, and right sides of a card. 
+         */
+        public static final int card_margin=0x7f070000;
+        /**  The amount of space between the icon and the phrase text on the result screen. 
+         */
+        public static final int result_icon_margin_right=0x7f070003;
+        /**  The amount of space between the game state circles and the timer display in the status
+         bar. 
+         */
+        public static final int timer_margin_left=0x7f070002;
+        /**  The distance between the bottom of a card and the bottom of a tips-view, adjusted so
+         that the baseline of the text is 40px from the bottom of the card. 
+         */
+        public static final int tip_margin_bottom=0x7f070001;
     }
     public static final class drawable {
         public static final int black_bg=0x7f020000;
         public static final int i_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_stop=0x7f020003;
+        public static final int ic_audio=0x7f020002;
+        public static final int ic_exit=0x7f020003;
+        public static final int ic_img=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_location_50=0x7f020006;
+        public static final int ic_stop=0x7f020007;
+        public static final int ic_txt=0x7f020008;
+        public static final int ic_vid=0x7f020009;
     }
     public static final class id {
-        public static final int Singer=0x7f090008;
-        public static final int bgPhoto=0x7f090002;
-        public static final int imageView=0x7f090001;
-        public static final int my_progressBar=0x7f090009;
-        public static final int queryLinearLayout=0x7f090003;
-        public static final int resultLinearLayout=0x7f090006;
-        public static final int stop=0x7f09000b;
-        public static final int surfaceView=0x7f090000;
-        public static final int tap_instruction=0x7f09000a;
-        public static final int text1=0x7f090004;
-        public static final int text2=0x7f090005;
-        public static final int titleOfWork=0x7f090007;
+        public static final int Singer=0x7f0a0008;
+        public static final int audio=0x7f0a000e;
+        public static final int bgPhoto=0x7f0a0002;
+        public static final int exit=0x7f0a0010;
+        public static final int imageView=0x7f0a0001;
+        public static final int img=0x7f0a000c;
+        public static final int my_progressBar=0x7f0a0009;
+        public static final int queryLinearLayout=0x7f0a0003;
+        public static final int resultLinearLayout=0x7f0a0006;
+        public static final int surfaceView=0x7f0a0000;
+        public static final int tap_instruction=0x7f0a000a;
+        public static final int text1=0x7f0a0004;
+        public static final int text2=0x7f0a0005;
+        public static final int tip_tap_for_options=0x7f0a000b;
+        public static final int titleOfWork=0x7f0a0007;
+        public static final int txt=0x7f0a000f;
+        public static final int vid=0x7f0a000d;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int avatar_main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int camcorder=0x7f050000;
+        public static final int camcorder_stop=0x7f050001;
+        public static final int camera_shutter=0x7f050002;
+        public static final int error=0x7f050003;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int glass_voice_trigger=0x7f060003;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060001;
+        public static final int audio=0x7f060009;
+        public static final int exit=0x7f06000b;
+        public static final int glass_voice_trigger=0x7f060004;
+        public static final int hello_world=0x7f060003;
+        public static final int img=0x7f060007;
+        public static final int lib_name=0x7f060000;
         /**  Menu item strings. 
          */
-        public static final int stop=0x7f060004;
-        public static final int tapforoptions=0x7f060005;
+        public static final int stop=0x7f060005;
+        /**  Tip text displayed at the bottom of the splash screen. 
+         */
+        public static final int tap_for_options=0x7f060006;
+        public static final int txt=0x7f06000a;
+        public static final int vid=0x7f060008;
     }
     public static final class style {
         /** 
@@ -70,8 +108,8 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070001;
-        public static final int MenuTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080001;
+        public static final int MenuTheme=0x7f080000;
     }
     public static final class xml {
         public static final int voice_trigger=0x7f040000;
--- src/com/darrenvenn/glasscamerasnapshot/MainActivity.java
+++ src/com/darrenvenn/glasscamerasnapshot/MainActivity.java
@@ -1,63 +1,65 @@
 package com.darrenvenn.glasscamerasnapshot;
 
 import java.io.File;
-import java.util.List;
-
-import com.google.android.glass.media.CameraManager;
+import com.google.android.glass.media.Sounds;
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
 
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.FileObserver;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 public class MainActivity extends Activity{
-	
-	// App responds to voice trigger "test the camera", takes a picture with GlassSnapshotActivity and then returns.
 	
 	private static final String TAG = MainActivity.class.getSimpleName();
 	private static final String FILE_NAME = "" + System.currentTimeMillis() + ".jpg";
 	private static final String IMAGE_FILE_PATH = Environment.getExternalStorageDirectory().getPath() + 
 													"/DCIM/Camera/" + FILE_NAME;
 
-	private boolean picTaken = false; // flag to indicate if we just returned from the picture taking intent
-	private TextView text1;
-	private TextView text2;
+	private final Handler mHandler = new Handler();
 	
-	private ProgressBar myProgressBar;
-	protected boolean mbActive;
-	
-	final Handler myHandler = new Handler(); // handles looking for the returned image file
-	private TextToSpeech mSpeech;
-    
-    private GestureDetector mGestureDetector;
-    
-	//Another location detection method
     LocationDetector myloc;
 	double myLat = 0;
 	double myLong = 0;
+	
+    private AudioManager mAudioManager;
+	
+	private final GestureDetector.BaseListener mBaseListener = new GestureDetector.BaseListener() {
+		@Override
+		public boolean onGesture(Gesture gesture) {
+			if (gesture == Gesture.TAP) {
+				mAudioManager.playSoundEffect(Sounds.TAP);
+				openOptionsMenu();
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
+	
+    private GestureDetector mGestureDetector;
+
+	
+	//Don't know if I really still need all of these
+//	private boolean picTaken = false; // flag to indicate if we just returned from the picture taking intent
+//	private TextView text1;
+//	private TextView text2;
+//	
+//	private ProgressBar myProgressBar;
+//	protected boolean mbActive;
+//	
+////	final Handler myHandler = new Handler(); // handles looking for the returned image file
+//	private TextToSpeech mSpeech;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -67,35 +69,20 @@ public class MainActivity extends Activity{
 		
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		
-		setContentView(R.layout.activity_main);
-        text1 = (TextView) findViewById(R.id.text1);
-        text2 = (TextView) findViewById(R.id.text2);
-        text1.setText("");
-        text2.setText("");
-        myProgressBar = (ProgressBar) findViewById(R.id.my_progressBar);
-        LinearLayout llResult = (LinearLayout) findViewById(R.id.resultLinearLayout);
-        TextView tvResult = (TextView) findViewById(R.id.tap_instruction);
-        llResult.setVisibility(View.INVISIBLE);
-        tvResult.setVisibility(View.INVISIBLE);
-		myProgressBar.setVisibility(View.INVISIBLE);
+		setContentView(R.layout.avatar_main);
 		
-
-		myloc = new LocationDetector(this);		
-        
-        // Even though the text-to-speech engine is only used in response to a menu action, we
-        // initialize it when the application starts so that we avoid delays that could occur
-        // if we waited until it was needed to start it up
-        mSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                // Do nothing.
-            }
-        });
-        
-        mGestureDetector = createGestureDetector(this);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mGestureDetector = new GestureDetector(this).setBaseListener(mBaseListener);
+		
+        myloc = new LocationDetector(this);	
 		
 	}
 
+	@Override
+	public boolean onGenericMotionEvent(MotionEvent event) {
+		return mGestureDetector.onMotionEvent(event);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -104,92 +91,45 @@ public class MainActivity extends Activity{
 	}
 	
 	@Override
-	protected void onResume() {
-		Log.v(TAG, "In onResume");
-		super.onResume();
-
-		if (!picTaken) {
-			//Temporary disable to test video
-			Intent intent = new Intent(this, GlassSnapshotActivity.class);
-	        intent.putExtra("imageFileName",IMAGE_FILE_PATH);
-	        intent.putExtra("previewWidth", 640);
-	        intent.putExtra("previewHeight", 360);
-	        intent.putExtra("snapshotWidth", 1920);
-	        intent.putExtra("snapshotHeight", 1080);
-	        intent.putExtra("maximumWaitTimeForCamera", 2000);
-		    startActivityForResult(intent,1);
-			
-			//Video intent works
-//			Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-//			startActivityForResult(intent, 2);
-			
-//			Log.v(TAG, "Before Intent");
-//			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//			startActivityForResult(intent, 3);
-//			Log.v(TAG, "After startActivityForResult");
-		}
-		else {
-			// do nothing
-		}
-	}
-	
-	/*
-     * Send generic motion events to the gesture detector
-     */
-    @Override
-    public boolean onGenericMotionEvent(MotionEvent event) {
-        if (mGestureDetector != null) {
-            return mGestureDetector.onMotionEvent(event);
-        }
-        return false;
-    }
-	
-	private GestureDetector createGestureDetector(Context context) {
-	    GestureDetector gestureDetector = new GestureDetector(context);
-	        //Create a base listener for generic gestures
-	        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
-	            @Override
-	            public boolean onGesture(Gesture gesture) {
-	                if (gesture == Gesture.TAP) {
-	                    // do something on tap
-	                	Log.v(TAG,"tap");
-	                	//if (readyForMenu) {
-	                		openOptionsMenu();
-	               		//}
-	                    return true;
-	                } else if (gesture == Gesture.TWO_TAP) {
-	                    // do something on two finger tap
-	                    return true;
-	                } else if (gesture == Gesture.SWIPE_RIGHT) {
-	                    // do something on right (forward) swipe
-	                    return true;
-	                } else if (gesture == Gesture.SWIPE_LEFT) {
-	                    // do something on left (backwards) swipe
-	                    return true;
-	                }
-	                return false;
-	            }
-	        });
-	        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
-	            @Override
-	            public void onFingerCountChanged(int previousCount, int currentCount) {
-	              // do something on finger count changes
-	            }
-	        });
-	        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
-	            @Override
-	            public boolean onScroll(float displacement, float delta, float velocity) {
-	                // do something on scrolling
-	            	return false;
-	            }
-	        });
-	        return gestureDetector;
-	    }
-
-	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.stop:
+        	case R.id.img:
+        		mHandler.post(new Runnable() {
+        			@Override
+        			public void run() {
+        				recordImage();
+        			}
+        		});
+        		return true;
+        		
+        	case R.id.vid:
+        		mHandler.post(new Runnable() {
+        			@Override
+        			public void run() {
+        				recordVideo();
+        			}
+        		});
+        		return true; 
+        		
+        	case R.id.audio:
+        		mHandler.post(new Runnable() {
+        			@Override
+        			public void run() {
+        				recordAudio();
+        			}
+        		});
+        		return true; 
+        		
+        	case R.id.txt:
+        		mHandler.post(new Runnable() {
+        			@Override
+        			public void run() {
+        				recordText();
+        			}
+        		});
+        		return true; 
+        		
+            case R.id.exit:
                 finish();
                 return true;
             default:
@@ -197,55 +137,43 @@ public class MainActivity extends Activity{
         }
     }
 	
+	private void recordImage() {
+		//TODO: Add a viewfinder, allow tap to capture, show picture after
+		Intent intent = new Intent(this, GlassSnapshotActivity.class);
+        intent.putExtra("imageFileName",IMAGE_FILE_PATH);
+        intent.putExtra("previewWidth", 640);
+        intent.putExtra("previewHeight", 360);
+        intent.putExtra("snapshotWidth", 1920);
+        intent.putExtra("snapshotHeight", 1080);
+        intent.putExtra("maximumWaitTimeForCamera", 2000);
+	    startActivityForResult(intent,1);
+	}
+	
+	private void recordVideo() {
+		//TODO: Allow for variable length, maybe show preview.
+		Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+		startActivityForResult(intent, 2);
+	}
+	
+	private void recordAudio() {
+		//Implement Later
+	}
+	
+	private void recordText() {
+		//Implement Later
+	}
+	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		Log.v(TAG, "In onActivityResult");
 		super.onActivityResult(requestCode, resultCode, data);
-		picTaken = true;
 		switch(requestCode) {
 	    	case (1) : {
 	    		if (resultCode == Activity.RESULT_OK) {
-	    			Log.v(TAG,"onActivityResult"); 
-	    	  
 	    			File f = new File(IMAGE_FILE_PATH);
 	    			if (f.exists()) {
 	    				Log.v(TAG,"image file from camera was found");
 	    				Log.v(TAG,"File direcotyr: " + f.getAbsolutePath());
-					   
-	    				//Gets image and previews it
-	    				Bitmap b = BitmapFactory.decodeFile(IMAGE_FILE_PATH);
-	    				Log.v(TAG,"bmp width=" + b.getWidth() + " height=" + b.getHeight());
-	    				ImageView image = (ImageView) findViewById(R.id.bgPhoto);
-	    				image.setImageBitmap(b);
-				      
-	    				//Sets text for card
-	    				text1 = (TextView) findViewById(R.id.text1);
-	    				text2 = (TextView) findViewById(R.id.text2);
-	    				text1.setText("The image shown was saved successfully to a file named:");
-	    				text2.setText("\n" + IMAGE_FILE_PATH);
-				      
-	    				//Sets layout and text on card
-	    				LinearLayout llResult = (LinearLayout) findViewById(R.id.resultLinearLayout);
-	    				llResult.setVisibility(View.VISIBLE);
-	    				TextView line1 = (TextView) findViewById(R.id.titleOfWork);
-	    				TextView line2 = (TextView) findViewById(R.id.Singer);
-	    				TextView tap = (TextView) findViewById(R.id.tap_instruction);
-	    				line1.setText("");
-	    				line2.setText("");
-	    				tap.setVisibility(View.VISIBLE);
-				      
-	//			  	    //Uploads to FTP server
-	//			  	    Log.v(TAG, "Starting  FTP upload");
-	//			  	    UploadToFTP ftp = new UploadToFTP();
-	//			  	    Log.v(TAG, "File Path: " + Environment.getExternalStorageDirectory()); 
-	//			  	    ftp.execute(IMAGE_FILE_PATH);
-	//			  	    Log.v(TAG, "Done FTP upload");
-				      
-	    				//Try to do the same with http
-	    				Log.v(TAG, "Starting POST upload");
-	    				UploadHttpPost client = new UploadHttpPost();
-	    				client.execute(IMAGE_FILE_PATH);
-			      
 	    			}
 	    			if (myloc.canGetLocation) {
 	    				myLat = myloc.getLatitude();
@@ -266,77 +194,12 @@ public class MainActivity extends Activity{
 	    		}
 	    		break;
 	    	}
-	    	case (3) : {
-	    		Log.v(TAG, "In case: 3");
-	    		if (resultCode == RESULT_OK) {
-	    			Log.v(TAG, "Picture response ok");
-	    			String picturePath = data.getStringExtra(CameraManager.EXTRA_PICTURE_FILE_PATH);
-	    			processPictureWhenReady(picturePath);
-	    		} else {
-	    			Log.v(TAG, "Picture response not ok");
-	    		}
-	    	}
 		}
 	}
 	
-	private void processPictureWhenReady(final String picturePath) {
-		Log.v(TAG, "In processPicture");
-		// TODO Auto-generated method stub
-		final File pictureFile = new File(picturePath);
-
-	    if (pictureFile.exists()) {
-	        // The picture is ready; process it.
-	    } else {
-	        // The file does not exist yet. Before starting the file observer, you
-	        // can update your UI to let the user know that the application is
-	        // waiting for the picture (for example, by displaying the thumbnail
-	        // image and a progress indicator).
-
-	        final File parentDirectory = pictureFile.getParentFile();
-	        FileObserver observer = new FileObserver(parentDirectory.getPath(),
-	                FileObserver.CLOSE_WRITE | FileObserver.MOVED_TO) {
-	            // Protect against additional pending events after CLOSE_WRITE
-	            // or MOVED_TO is handled.
-	            private boolean isFileWritten;
-
-	            @Override
-	            public void onEvent(int event, String path) {
-	                if (!isFileWritten) {
-	                    // For safety, make sure that the file that was created in
-	                    // the directory is actually the one that we're expecting.
-	                    File affectedFile = new File(parentDirectory, path);
-	                    isFileWritten = affectedFile.equals(pictureFile);
-
-	                    if (isFileWritten) {
-	                        stopWatching();
-
-	                        // Now that the file is ready, recursively call
-	                        // processPictureWhenReady again (on the UI thread).
-	                        runOnUiThread(new Runnable() {
-	                            @Override
-	                            public void run() {
-	                                processPictureWhenReady(picturePath);
-	                            }
-	                        });
-	                    }
-	                }
-	            }
-	        };
-	        observer.startWatching();
-	    }
-	    Log.v(TAG, "End of processPicture");
-	}
 
 	@Override
     protected void onDestroy() {
-
-        //Close the Text to Speech Library
-        if(mSpeech != null) {
-            mSpeech.stop();
-            mSpeech.shutdown();
-            mSpeech = null;
-            Log.d(TAG, "TTS Destroyed");
-        }
         super.onDestroy();
     }	
 }
